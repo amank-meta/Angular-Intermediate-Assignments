be13fc022128ad4a0307cd50fa1cc2c802f777b3
amank-meta <aman.khandelwal@metacube.com>
2023-02-01 14:42:58 +0530

    Commented

--- src/app/attribute.directive.spec.ts
+++ src/app/attribute.directive.spec.ts
@@ -1,8 +1,8 @@
-import { AttributeDirective } from './attribute.directive';
+// import { AttributeDirective } from './attribute.directive';
 
-describe('AttributeDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AttributeDirective();
-    expect(directive).toBeTruthy();
-  });
-});
+// describe('AttributeDirective', () => {
+//   it('should create an instance', () => {
+//     const directive = new AttributeDirective();
+//     expect(directive).toBeTruthy();
+//   });
+// });
